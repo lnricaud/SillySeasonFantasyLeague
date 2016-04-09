8b446e6dd039e8a3840978a3a3c9856a7824d3b4
larskris <wondringaloud@hotmail.com>
2016-04-08 22:48:43 -0700

    added link on landing page to angular2 client

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-    @user = current_user
-    if @user
-      render json: {isSignedIn: true, user: @user.name}
-    else
-      render json: {isSignedIn: false}
-    end
+    # render welcome view here, view should have Proceed to App button and admin controlls
+    render :index
   end
 
 end
