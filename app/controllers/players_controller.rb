c08547d36e5be40b927d34cadbfc5e3c1b1ccf9a
larskris <wondringaloud@hotmail.com>
2016-04-12 12:04:36 -0700

    made route accessible only by admin

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,37 +1,45 @@
 class PlayersController < ApplicationController
+	before_action :authenticate
+
 	def refresh
-		require 'open-uri'
-		require 'json'
-		i = 1
-		loop do
-			begin
-			  player_data = open("http://fantasy.premierleague.com/web/api/elements/#{i}/").read  
-			rescue OpenURI::HTTPError   
-			  p "Found last element, breaking out of loop"
-			  break  
-			end  
-			# p player_data["id"]
-			db_data = Playerdata.find_by_id(i)
-			if db_data.nil?
-				p "Creating new Playerdata"
-				Playerdata.create(data: player_data)
-				p "Adding new Player to all Leagues"
-				Log.create(action: "newPlayer", game_week: current_gameweek, player_id: i)
-				1.upto(League.count) do |l|
-					pl = Player.create({league_id: l, playerdata_id: i, value: 4000000})
-					p "Created player: #{pl[:id]}"
+		user = current_user
+		if user.admin
+			require 'open-uri'
+			require 'json'
+			i = 1
+			loop do
+				begin
+				  player_data = open("http://fantasy.premierleague.com/web/api/elements/#{i}/").read  
+				rescue OpenURI::HTTPError   
+				  p "Found last element, breaking out of loop"
+				  break  
+				end  
+				# p player_data["id"]
+				db_data = Playerdata.find_by_id(i)
+				if db_data.nil?
+					p "Creating new Playerdata"
+					Playerdata.create(data: player_data)
+					p "Adding new Player to all Leagues"
+					Log.create(action: "newPlayer", game_week: current_gameweek, player_id: i)
+					1.upto(League.count) do |l|
+						pl = Player.create({league_id: l, playerdata_id: i, value: 4000000})
+						p "Created player: #{pl[:id]}"
+					end
+				else
+					p "Uppdating existing playerdata.data"
+					updated_attributes = {:data => player_data}
+					db_data.update_attributes(updated_attributes)
 				end
-			else
-				p "Uppdating existing playerdata.data"
-				updated_attributes = {:data => player_data}
-				db_data.update_attributes(updated_attributes)
+				i += 1
 			end
-			i += 1
+			# byebug
+			parsedata
+			render json: {response: 'Player Data Refreshed'}
+		else
+			render json: {err: 'Not Autherized to Refresh Player Data'}, status: 401
 		end
-		# byebug
-		parsedata
-		redirect_to "/" 
 	end
+
 	def players
 		user = current_user
 		players = Player.where(league_id: user.league_id)
